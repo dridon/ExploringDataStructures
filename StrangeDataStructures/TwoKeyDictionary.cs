b03bd8aee15d0475805116adc83b719b0b611e8f
dridon <dridon@gmail.com>
2013-09-26 00:42:08 -0400

    TwoKeyDictionary is now working

---
+++ StrangeDataStructures/TwoKeyDictionary.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrangeDataStructures
+{
+    /// <summary>
+    /// A two key dictionary class useful for classifying a set A that has two disjoint sets B and C 
+    /// that have a 1:1 mapping to it. For example an employee number or an employee name may both
+    /// can both uniquely identify an employee profile. 
+    /// </summary>
+    /// <typeparam name="S"> Type of the first set of keys </typeparam>
+    /// <typeparam name="T"> Type of the second set of keys </typeparam>
+    /// <typeparam name="R"> Type of the value to be retrieved</typeparam>
+    public class TwoKeyDictionary<S,T,R>
+    {
+        #region Properties
+        // The dictionary is actually an implementation of two other dictionaries 
+        Dictionary<S, TwoKeyContainer> Dictionary1 { get; set; }
+        Dictionary<T, TwoKeyContainer> Dictionary2 { get; set; }
+        #endregion Properties 
+
+        #region Constructors
+        /// <summary>
+        /// Initializes the TwoKeyDictionary 
+        /// </summary>
+        public TwoKeyDictionary()
+        {
+            Dictionary1 = new Dictionary<S, TwoKeyContainer>();
+            Dictionary2 = new Dictionary<T, TwoKeyContainer>();
+        }
+        #endregion Constructors
+
+        #region Methods 
+        /// <summary>
+        /// Adds val to the TwoKeyDictionary with key1 and key2 pointing to it. 
+        /// If key1 and key2 two exist already and point to the same value then the value is overridden. 
+        /// If only key1 or key2 exist or both exist but point to separate values an 
+        /// ArgumentException is thrown
+        /// </summary>
+        /// <param name="key1"> The first key pointing to val</param>
+        /// <param name="key2"> The second key pointing to val</param>
+        /// <param name="val"> The value for the keys</param>
+        public void Add(S key1, T key2, R val)
+        {
+            // ensure the keys aren't already in use for unequal values 
+            if(!AreKeysValid(key1, key2)) 
+                throw new ArgumentException("One or more keys already exist and point to separate values");
+            
+            // initialize the container and load the dictionaries 
+            TwoKeyContainer container = new TwoKeyContainer(key1, key2, val);
+            Dictionary1[key1] = container; 
+            Dictionary2[key2] = container;
+        }
+
+        // key1 and key2 both have to exist in Dictionry1 and Dictionary2 together or 
+        // neither have to exist for the keys to be valid 
+        private bool AreKeysValid(S key1, T key2)
+        {
+            // logical biconditional of containment has to be true for the keys to be valid along with 
+            // the keys pointing to the same objects 
+            return (Dictionary1.ContainsKey(key1) && Dictionary2.ContainsKey(key2) && Dictionary1[key1] == Dictionary2[key2])
+                || (!Dictionary1.ContainsKey(key1) && !Dictionary2.ContainsKey(key2));
+        }
+
+        /// <summary>
+        /// Returns a value if it exists for the given key. Returns the default value for 
+        /// the type otherwise. 
+        /// </summary>
+        /// <param name="key"> The key that maps to the value</param>
+        /// <returns> The value if it exists. Default of its type if it does not.</returns>
+        public R Get1(S key)
+        {
+            if (Dictionary1.ContainsKey(key)) 
+                return Dictionary1[key].Value;
+            return default(R);
+        }
+
+        /// <summary>
+        /// Returns a value if it exists for the given key. Returns the default value for 
+        /// the type otherwise. 
+        /// </summary>
+        /// <param name="key"> The key that maps to the value</param>
+        /// <returns> The value if it exists. Default of its type if it does not.</returns>
+        public R Get2(T key)
+        {
+            if (Dictionary2.ContainsKey(key))
+                return Dictionary2[key].Value;
+            return default(R);
+        }
+
+        /// <summary>
+        /// Removes both keys and the value associated with this key 
+        /// </summary>
+        /// <param name="key">Key mapping to the value to be removed</param>
+        /// <returns>true if successfully removed. false if the key did not exist</returns>
+        public bool Remove1(S key)
+        {
+            if (Dictionary1.ContainsKey(key))
+            {
+                TwoKeyContainer container = Dictionary1[key];
+                return Dictionary1.Remove(key) && Dictionary2.Remove(container.Key2);
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Removes both keys and the value associated with this key
+        /// </summary>
+        /// <param name="key">Key mapping to the value to be removed</param>
+        /// <returns>true if successfully removed. false if the key did not exist</returns>
+        public bool Remove2(T key)
+        {
+            if (Dictionary2.ContainsKey(key))
+            {
+                TwoKeyContainer container = Dictionary2[key];
+                return Dictionary1.Remove(container.Key1) && Dictionary2.Remove(key);
+            }
+            return false;
+        }
+
+        #endregion Methods
+
+        #region Classes
+        // Facilitates the removal with only one key 
+        class TwoKeyContainer
+        {
+            // the keys and values in  
+            public S Key1 { get; private set; }
+            public T Key2 { get; private set; }
+            public R Value { get; private set; }
+
+            // initialize an immutable container  
+            public TwoKeyContainer(S key1, T key2, R val)
+            {
+                Key1 = key1;
+                Key2 = key2;
+                Value = val;
+            }
+
+            // structural equality 
+            public override bool Equals(object obj)
+            {
+ 	             if(obj  is TwoKeyContainer)
+                 {
+                     TwoKeyContainer twoKC = (TwoKeyContainer) obj;
+                     return Key1.Equals(twoKC.Key1) && Key2.Equals(twoKC.Key2) && Value.Equals(twoKC.Value);
+                 }
+                return false; 
+            }
+
+            // hash codes are structurally created 
+            public override int GetHashCode()
+            {
+                int hash = 13; 
+                hash = hash*7 + Key1.GetHashCode(); 
+                hash = hash*7 + Key2.GetHashCode();
+                hash = hash*7 + Value.GetHashCode();
+                return hash;
+            }
+        }
+        #endregion Classes
+    }
+}
---
+++ StrangeDataStructuresTest/TwoKeyDictionaryTest.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using StrangeDataStructures;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace StrangeDataStructuresTest
+{
+    /// <summary>
+    /// Tests the majority of the methods in the TwoKeyDictionary 
+    /// </summary>
+    [TestClass]
+    public class TwoKeyDictionaryTest
+    {
+        #region Test Methods
+        /// <summary>
+        /// Tests that elements are successfully added and
+        /// retrieved from the TwoKeyDictionary
+        /// </summary>
+        [TestMethod]
+        public void TestSuccessfulAddAndRetrieve()
+        {
+            Employee e1 = new Employee("Emma", "emma.powers@mindset.com", "1105010");
+            Employee e2 = new Employee("Han", "han.yun@mindset.com", "5530223");
+
+            TwoKeyDictionary<String, String, Employee> twoKC = new TwoKeyDictionary<String, String, Employee>();
+            twoKC.Add(e1.ID, e1.Email, e1);
+            twoKC.Add(e2.ID, e2.Email, e2);
+
+            // check for employee 1 
+            Assert.AreEqual(twoKC.Get1(e1.ID), e1);
+            Assert.AreEqual(twoKC.Get2(e1.Email), e1);
+
+            // check for employee 2 
+            Assert.AreEqual(twoKC.Get1(e2.ID), e2);
+            Assert.AreEqual(twoKC.Get2(e2.Email), e2);
+        }
+
+        /// <summary>
+        /// Tests to ensure that the cases that should not retrieve anything
+        /// indeed do not in the TwoKeyDictionary
+        /// </summary>
+        [TestMethod]
+        public void TestFailedAddAndRetrieve()
+        {
+            
+            Employee e1 = new Employee("Priya", "priya.naria@footloose.com", "8868");
+            Employee e2 = new Employee("Bruno", "burno.venus@footloose.com", "1324");
+
+            TwoKeyDictionary<String, String, Employee> twoKC = new TwoKeyDictionary<String, String, Employee>();
+            twoKC.Add(e1.ID, e1.Email, e1);
+
+            // cann not retrieve from the wrong key
+            Assert.IsNull(twoKC.Get2(e1.ID));       
+           
+            // ensure a non-existing object has a  default return 
+            Assert.IsNull(twoKC.Get1(e2.ID));            
+        }
+
+        /// <summary>
+        /// Tests to see if the remove methods work correctly in the TwoKeyDictionary
+        /// </summary>
+        [TestMethod]
+        public void TestRemove()
+        {
+            Employee e1 = new Employee("Priya", "priya.naria@footloose.com", "8868");
+            Employee e2 = new Employee("Bruno", "burno.venus@footloose.com", "1324");
+
+            TwoKeyDictionary<String, String, Employee> twoKC = new TwoKeyDictionary<String, String, Employee>();
+            twoKC.Add(e1.ID, e1.Email, e1);
+            twoKC.Add(e2.ID, e2.Email, e2);
+
+            // test Remove1 and ensure Remove2 doesn't find an object to remove 
+            Assert.IsTrue(twoKC.Remove1(e1.ID));
+            Assert.IsFalse(twoKC.Remove2(e1.Email));
+
+            // test Remove2 and ensure Remove1 doesn't find an object to remove 
+            Assert.IsTrue(twoKC.Remove2(e2.Email));
+            Assert.IsFalse(twoKC.Remove1(e2.ID));            
+        }
+
+        /// <summary>
+        /// Ensures that the ArgumentException in the TwoKeyDictionary is behaving 
+        /// properly 
+        /// </summary>
+        [TestMethod]
+        public void TestException()
+        {
+            Employee e1 = new Employee("Priya", "priya.naria@footloose.com", "8868");
+            Employee e2 = new Employee("Priya2", "priya.naria@footloose.com", "8868");
+            Employee e3 = new Employee("Priya3", "priya.naria@footloose.com", "8869");
+
+            TwoKeyDictionary<String, String, Employee> twoKC = new TwoKeyDictionary<String, String, Employee>();
+            twoKC.Add(e1.ID, e1.Email, e1);
+            try
+            {
+                // this should pass since its an override 
+                twoKC.Add(e2.ID, e2.Email, e2);
+            }
+            catch (ArgumentException e)
+            {
+
+                // getting here suggests overriding failed
+                Assert.Fail();
+            }
+            try
+            {
+                // this should raise an exception since one of the keys already points to someone else
+                twoKC.Add(e3.ID, e3.Email, e3);                
+                Assert.Fail();
+            }
+            catch (ArgumentException ae){ /*Test passes here*/}
+        }
+        #endregion Test Methods
+
+        #region Classes
+        // Test class for two key dictionary
+        class Employee
+        {
+            public String Name { get; private set; }
+            public String Email { get; private set; }
+            public String ID { get; private set; }
+
+            // initialize as immutable object 
+            public Employee(String name, String email, String id)
+            {
+                Name = name; 
+                Email = email; 
+                ID = id;
+            }
+
+            // structural equality 
+            public override bool Equals(object obj)
+            {
+                if(obj is Employee)
+                {
+                    Employee e = (Employee) obj;
+                    return Name == e.Name && Email == e.Email && ID == e.ID;
+                }
+                return false;
+            }
+
+            // hash codes are structurally created 
+            public override int GetHashCode()
+            {
+                int hash = 13;
+                hash = hash * 7 + Name.GetHashCode();
+                hash = hash * 7 + Email.GetHashCode();
+                hash = hash * 7 + ID.GetHashCode();
+                return hash; 
+            }
+        }
+        #endregion Classes
+    }
+}
